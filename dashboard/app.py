a7207519ad00441d68b0eba7d6898e0e7197aa8b
Juan M. Barrios <j.m.barrios@gmail.com>
2026-09-23 09:29:57 -0600

    fix: corregir liga a externa en Dash para navegar a paginas del sitio estatico

--- dashboard/app.py
+++ dashboard/app.py
@@ -252,10 +252,10 @@ def make_layout ():
         dbc.NavbarSimple(
             
             children=[
-                dbc.NavItem(dbc.NavLink("Home", href=f"{STATIC_SITE_BASE_URL}/" if STATIC_SITE_BASE_URL else "/", style={'padding-left':'10px','margin-top':'5px','margin-bottom':'5px'})),
+                dbc.NavItem(dbc.NavLink("Home", href=f"{STATIC_SITE_BASE_URL}/" if STATIC_SITE_BASE_URL else "/", external_link=True, style={'padding-left':'10px','margin-top':'5px','margin-bottom':'5px'})),
                 dbc.NavItem(dbc.NavLink("Mapa", href=f"{DASHBOARD_REQUESTS_PATHNAME_PREFIX}", active=True, style={'padding-left':'10px','margin-top':'5px','margin-bottom':'5px',})),
-                dbc.NavItem(dbc.NavLink("Ayuda", href=f"{STATIC_SITE_BASE_URL}/#ayuda" if STATIC_SITE_BASE_URL else "/#ayuda", style={'padding-left':'10px','margin-top':'5px','margin-bottom':'5px'})),
-                dbc.NavItem(dbc.NavLink("Créditos", href=f"{STATIC_SITE_BASE_URL}/#creditos" if STATIC_SITE_BASE_URL else "/#creditos", style={'padding-left':'10px','margin-top':'5px','margin-bottom':'5px'})),
+                dbc.NavItem(dbc.NavLink("Ayuda", href=f"{STATIC_SITE_BASE_URL}/#ayuda" if STATIC_SITE_BASE_URL else "/#ayuda", external_link=True, style={'padding-left':'10px','margin-top':'5px','margin-bottom':'5px'})),
+                dbc.NavItem(dbc.NavLink("Créditos", href=f"{STATIC_SITE_BASE_URL}/#creditos" if STATIC_SITE_BASE_URL else "/#creditos", external_link=True, style={'padding-left':'10px','margin-top':'5px','margin-bottom':'5px'})),
                 
             ],
             color="dark",
